75ee62631238a1e3a81458ba6a0d7e896a74e17c
Akos Kemives <akoskm@gmail.com>
2016-11-05 13:10:07 +0100

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -8,7 +8,7 @@ class Main extends React.Component {
       <Header />
       <p>
         Change some of the files in src/components and watch the
-        components autoreload without causing a full page reload.
+        components self-reload without causing a full page reload.
       </p>
     </div>
   }
